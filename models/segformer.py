9ea9f3efc381ace79a86a63fbf2ce582ba7fc0e0
John Doe <johndoe@example.com>
2024-05-04 20:52:38 +0530

    commn

--- models/segformer.py
+++ models/segformer.py
@@ -45,7 +45,7 @@ def SegFormer_B0(input_shape, num_classes):
         embed_dims=MODEL_CONFIGS["mit_b0"]["embed_dims"],
         depths=MODEL_CONFIGS["mit_b0"]["depths"],
     )(pixel_values)
-    x = layers.x = layers.BatchNormalization()(x)(x)
+    x = layers.BatchNormalization()(x)(x)
     x = SegFormerHead(
         num_classes=num_classes,
         decode_dim=MODEL_CONFIGS["mit_b0"]["decode_dim"],
@@ -63,7 +63,7 @@ def SegFormer_B1(input_shape, num_classes):
         embed_dims=MODEL_CONFIGS["mit_b1"]["embed_dims"],
         depths=MODEL_CONFIGS["mit_b1"]["depths"],
     )(pixel_values)
-    x = layers.x = layers.BatchNormalization()(x)(x)
+    x = layers.BatchNormalization()(x)
     x = SegFormerHead(
         num_classes=num_classes,
         decode_dim=MODEL_CONFIGS["mit_b1"]["decode_dim"],
@@ -81,7 +81,7 @@ def SegFormer_B2(input_shape, num_classes):
         embed_dims=MODEL_CONFIGS["mit_b2"]["embed_dims"],
         depths=MODEL_CONFIGS["mit_b2"]["depths"],
     )(pixel_values)
-    x = layers.x = layers.BatchNormalization()(x)(x)
+    x = layers.BatchNormalization()(x)
     x = SegFormerHead(
         num_classes=num_classes,
         decode_dim=MODEL_CONFIGS["mit_b2"]["decode_dim"],
@@ -99,12 +99,12 @@ def SegFormer_B3(input_shape, num_classes):
         embed_dims=MODEL_CONFIGS["mit_b3"]["embed_dims"],
         depths=MODEL_CONFIGS["mit_b3"]["depths"],
     )(pixel_values)
-    x = layers.x = layers.BatchNormalization()(x)(x)
+    x = layers.BatchNormalization()(x)
     x = SegFormerHead(
         num_classes=num_classes,
         decode_dim=MODEL_CONFIGS["mit_b3"]["decode_dim"],
     )(x)
-
+    x = tf.keras.layers.LayerNormalization(input_shape=x.shape[1:])(x)
     x = ResizeLayer(input_shape[0], input_shape[1])(x)
     x = tf.keras.activations.softmax(x,)
     return tf.keras.Model(inputs=pixel_values, outputs=x)
@@ -117,7 +117,7 @@ def SegFormer_B4(input_shape, num_classes):
         embed_dims=MODEL_CONFIGS["mit_b4"]["embed_dims"],
         depths=MODEL_CONFIGS["mit_b4"]["depths"],
     )(pixel_values)
-    x = layers.x = layers.BatchNormalization()(x)(x)
+    x = layers.BatchNormalization()(x)
     x = SegFormerHead(
         num_classes=num_classes,
         decode_dim=MODEL_CONFIGS["mit_b4"]["decode_dim"],
@@ -135,7 +135,7 @@ def SegFormer_B5(input_shape, num_classes):
         embed_dims=MODEL_CONFIGS["mit_b5"]["embed_dims"],
         depths=MODEL_CONFIGS["mit_b5"]["depths"],
     )(pixel_values)
-    x = layers.x = layers.BatchNormalization()(x)(x)
+    x = layers.BatchNormalization()
     x = SegFormerHead(
         num_classes=num_classes,
         decode_dim=MODEL_CONFIGS["mit_b5"]["decode_dim"],
